6bee00a178a386ddcd4d62598fd11fa904e3ffe1
Gustavo-Marsola <gustavo.marsola@solfacil.com.br>
2024-01-23 09:32:06 -0300

    func 1

---
+++ Python Básico/1 - Funções Primordiais.py
@@ -0,0 +1,10 @@
+#Receber via terminal um valor para a minha variavel
+
+nome = input('Digite o seu nome: ')
+
+#Exibir no terminal o valor da variavel
+print('Seu nome é: ', nome)
+
+#Verificar qual é o tipo de variavel que estamos trabalhando
+
+print('A variavel nome é do tipo: ', type(nome))
