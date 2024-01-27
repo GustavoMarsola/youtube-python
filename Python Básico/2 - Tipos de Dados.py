3064d56ae8be0ba694bc763717f609995b05143f
Gustavo-Marsola <gustavo.marsola@solfacil.com.br>
2024-01-27 13:57:14 -0300

    tipos de dados

---
+++ Python Básico/2 - Tipos de Dados.py
@@ -0,0 +1,16 @@
+# Descrição de uma pessoa aleatória
+
+# Strings (str) -> Representam cadeias de caracteres
+nome = "Fulano da Silva Ciclano"
+# Inteiros (int) -> Representam numeros inteiros
+idade = 35
+# Float (float) -> Representam os numeros reais (com o ponto separador de decimal)
+altura = 1.82
+# Booleanos (bool) -> Representam valores lógicos (verdadeiro e falso)
+casado = True
+solteiro = False
+# Nulos (None) -> Ausência de valor significativo
+# Quando uma característica não se aplica, ou é opcional informar
+bigode = None
+cabelo = None
+
