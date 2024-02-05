d3e674b5e5f36bb8096ac1b00b6d65848362c95e
GustavoMarsola <gmarsola@gmail.com>
2024-02-05 16:32:53 -0300

    d

---
+++ Python Básico/C - Strings.py
@@ -0,0 +1,7 @@
+# Funções bem úteis das strings
+
+frase_errada = 'o CORINTHIANS é o melhor time do BRASIL'
+
+frase_correta = frase_errada.replace('CORINTHIANS', 'SÃO PAULO')
+
+print('Agora sim está correto: ', frase_correta.upper())
