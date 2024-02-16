9dfea834ef4f76b26a49d059a292d16f94071d92
Gustavo-Marsola <gustavo.marsola@solfacil.com.br>
2024-02-16 08:01:13 -0300

    strings

--- Python Básico/C - Strings.py
+++ Python Básico/C - Strings.py
@@ -4,15 +4,26 @@
 
 frase_errada = 'O CORINTHIANS É O MELHOR TIME DO BRASIL'
 frase_correta = frase_errada.replace('CORINTHIANS', 'SÃO PAULO')
-print(frase_correta)
+# print(frase_correta)
 
 
 # 2) isnumeric
 
 cpf = '123.456.789-00'
 
-print(cpf.isnumeric()) # False, pois tem pontos e hífen
+# print(cpf.isnumeric()) # False, pois tem pontos e hífen
 
 cpf = '12345678900'
 
-print(cpf.isnumeric()) # True, pois contém somente números
+# print(cpf.isnumeric()) # True, pois contém somente números
+
+# 3) lower e upper
+
+cidade1 = "Osasco"    
+cidade2 = "OSASCO"
+
+conferir_cidades = cidade1 == cidade2
+
+print('É a mesma cidade: ', conferir_cidades)
+print('Cidade 1: ', cidade1)
+print('Cidade 2: ', cidade2)
